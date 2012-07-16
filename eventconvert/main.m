87bc4ae239d281246b8cd54c623778c1b626eb2c
Chris Haugli <chris@nowbox.com>
2012-07-16 15:44:55 -0700

    Add eventconvert target and rename project to JSONConverters.

---
+++ eventconvert/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  eventconvert
+//
+//  Created by Chris Haugli on 7/16/12.
+//  Copyright (c) 2012 Pipely Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
